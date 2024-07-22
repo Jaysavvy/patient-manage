676d25fc7f533621951c0f13dcf9e6b8726b801c
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2024-07-22 11:45:15 -0400

    Change vite rm glob

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,15 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import glob from 'glob';
-import path from 'path';
-
-
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: [
-//                 'resources/css/app.css',
-//                 'resources/js/app.js',
-//                 // Include additional assets as needed
-//                 'resources/assets/icons/Cancelled.svg'
-//             ],
-//             refresh: true,
-//         }),
-//     ],
-// });
-
-function getAllFiles(dirPath, arrayOfFiles) {
-    const files = glob.sync(`${dirPath}/**/*.*`);
-
-    return files.reduce((acc, file) => {
-        const ext = path.extname(file);
-        if (['.js', '.css'].includes(ext)) {
-            acc.push(file);
-        }
-        return acc;
-    }, arrayOfFiles);
-}
-
-const inputFiles = getAllFiles('resources', []);
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: inputFiles,
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                // Include additional assets as needed
+                'resources/assets/icons/Cancelled.svg'
+            ],
             refresh: true,
         }),
     ],
